1cf9d110f743631821761d4a459f7c12cf622a95
MD Dubey <dubeyg@whatever.com>
2014-02-15 12:35:56 +0530

    Different Panels added for putting control of same type.

--- src/com/tw/unoProject/view/PlayerScreen.java
+++ src/com/tw/unoProject/view/PlayerScreen.java
@@ -4,8 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PlayerScreen extends JFrame {
-    private JPanel pnlWindow;
-    private JPanel pnlPlayers;
+    private JPanel homePanel;
+    private JPanel playersPanel;
+    private JPanel centerPanel;
+    private JButton drawButton;
 
     public PlayerScreen() {
         super("UNO");
@@ -13,30 +15,46 @@ public class PlayerScreen extends JFrame {
     }
 
     private void generateUI() {
-        pnlWindow = new JPanel();
-        setContentPane(pnlWindow);
+        homePanel = new JPanel();
+        setContentPane(homePanel);
+        homePanel.setBackground(Color.GRAY);
+        homePanel.setLayout(null);
         setSize(1000, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        addPalyerPanel(pnlWindow);
-        pnlWindow.setBackground(Color.GRAY);
-        pnlWindow.setLayout(null);
-        pnlPlayers.setLayout(new GridLayout(1,5));
+        addPalyerPanel();
+        addCenterPanel();
+        createCatchButtons();
+        createDrawButton();
+    }
 
+    private void addCenterPanel() {
+        centerPanel = new JPanel();
+        centerPanel.setLayout(null);
+        centerPanel.setBounds(200, 150, 400, 200);
+        centerPanel.setBackground(Color.white);
+        homePanel.add(centerPanel);
+    }
+
+    private void createDrawButton() {
+        drawButton = new JButton("Draw");
+        drawButton.setBounds(15, 15, 150, 50);
+        centerPanel.add(drawButton);
+    }
+
+    private void createCatchButtons() {
         for (int i = 0; i < 5; i++) {
             JButton button = new JButton(String.valueOf(i));
-            pnlPlayers.add(button);
+            playersPanel.add(button);
         }
     }
 
-    private void addPalyerPanel(JPanel pnlWindow) {
-        pnlPlayers = new JPanel();
-        pnlPlayers.setBounds(30,30,700,30);
-        pnlPlayers.setVisible(true);
-        System.out.println(pnlPlayers.getBounds());
-        pnlPlayers.setBackground(Color.BLACK);
-        pnlWindow.add(pnlPlayers);
+    private void addPalyerPanel() {
+        playersPanel = new JPanel();
+        playersPanel.setBounds(30, 30, 700, 30);
+        playersPanel.setLayout(new GridLayout(1, 5));
+        homePanel.add(playersPanel);
     }
 
 
