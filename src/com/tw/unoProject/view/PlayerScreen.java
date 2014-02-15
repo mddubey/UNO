8e61cecf0ba581c0ba781679cee7d0862ba2fed2
MD Dubey <dubeyg@whatever.com>
2014-02-15 13:58:20 +0530

    Open Pile card is now in central panel. But has to reformat somthing.

--- src/com/tw/unoProject/view/PlayerScreen.java
+++ src/com/tw/unoProject/view/PlayerScreen.java
@@ -4,10 +4,11 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PlayerScreen extends JFrame {
-    private JPanel homePanel;
+    private JPanel playerMasterPanel;
     private JPanel playersPanel;
     private JPanel centerPanel;
     private JButton drawButton;
+    private JPanel cardPanel;
 
     public PlayerScreen() {
         super("UNO");
@@ -15,18 +16,34 @@ public class PlayerScreen extends JFrame {
     }
 
     private void generateUI() {
-        homePanel = new JPanel();
-        setContentPane(homePanel);
-        homePanel.setBackground(Color.GRAY);
-        homePanel.setLayout(null);
+        playerMasterPanel = new JPanel();
+        setContentPane(playerMasterPanel);
+        playerMasterPanel.setBackground(Color.GRAY);
+        playerMasterPanel.setLayout(null);
         setSize(1000, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        addPalyerPanel();
-        addCenterPanel();
+        addPlayerPanel();
         createCatchButtons();
+
+        addCenterPanel();
         createDrawButton();
+        showOpenedPileCard();
+    }
+
+    private void addPlayerPanel() {
+        playersPanel = new JPanel();
+        playersPanel.setBounds(30, 30, 700, 30);
+        playersPanel.setLayout(new GridLayout(1, 5));
+        playerMasterPanel.add(playersPanel);
+    }
+
+    private void createCatchButtons() {
+        for (int i = 0; i < 10; i++) {
+            JButton button = new JButton(String.valueOf(i + 1));
+            playersPanel.add(button);
+        }
     }
 
     private void addCenterPanel() {
@@ -34,7 +51,7 @@ public class PlayerScreen extends JFrame {
         centerPanel.setLayout(null);
         centerPanel.setBounds(200, 150, 400, 200);
         centerPanel.setBackground(Color.white);
-        homePanel.add(centerPanel);
+        playerMasterPanel.add(centerPanel);
     }
 
     private void createDrawButton() {
@@ -43,19 +60,16 @@ public class PlayerScreen extends JFrame {
         centerPanel.add(drawButton);
     }
 
-    private void createCatchButtons() {
-        for (int i = 0; i < 5; i++) {
-            JButton button = new JButton(String.valueOf(i));
-            playersPanel.add(button);
-        }
+    private void showOpenedPileCard() {
+        cardPanel = new JPanel();
+        cardPanel.setLayout(null);
+        cardPanel.setBounds(200, 15, 150, 50);
+        cardPanel.setBackground(Color.cyan);
+        centerPanel.add(cardPanel);
+        JLabel openPileCard = new JLabel("1");
+        openPileCard.setFont(new Font("Serif", Font.BOLD, 30));
+        openPileCard.setForeground(Color.black);
+        openPileCard.setBounds(20, 5, 100, 30);
+        cardPanel.add(openPileCard);
     }
-
-    private void addPalyerPanel() {
-        playersPanel = new JPanel();
-        playersPanel.setBounds(30, 30, 700, 30);
-        playersPanel.setLayout(new GridLayout(1, 5));
-        homePanel.add(playersPanel);
-    }
-
-
 }
